bdd24e98417dd885491afe453e5039d5cee32913
Willian <w.goncalves91@gmail.com>
2017-05-09 08:34:14 -0300

    Config action mailer default_url_options.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  host = 'http://staging-wwp.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.asset_host = host
 end
