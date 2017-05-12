7b52aac1424771dd1ef9ca3c85982f6b26726f6e
Willian <w.goncalves91@gmail.com>
2017-05-11 21:20:44 -0300

    Add comment for future implementation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    # groups_path if current_user.last_group.nil?
+    # group_path(current_user.last_group)
+  end
+
   protected
 
   def configure_permitted_parameters
