12b01949e9d5948e7f22c74a6b5093ce1ffabf04
Willian <w.goncalves91@gmail.com>
2017-05-11 21:21:01 -0300

    Implement tests.

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
-  
+  context 'unauthenticated user' do
+    describe 'GET #index' do
+      before { get :index }
+      it { expect(response).to redirect_to user_session_path }
+    end
+  end
+
+  context 'authenticated user' do
+    let(:user) { Fabricate :user }
+
+    before { sign_in user }
+
+    describe 'GET #index' do
+      before { get :index }
+      it { expect(response.status).to be(200) }
+    end
+  end
 end
