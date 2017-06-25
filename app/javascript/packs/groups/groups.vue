7f55a060190ab35bff144d05f4c89535b9f4910d
Willian <w.goncalves91@gmail.com>
2017-06-24 22:46:30 -0300

    Fix transition name.

--- app/javascript/packs/groups/groups.vue
+++ app/javascript/packs/groups/groups.vue
@@ -4,7 +4,7 @@
       {{ title }}
       <button class="btn-floating btn-flat waves-effect waves-light right" @click="expand = !expand" :class="{ 'arrow-down': !expand }"><i class="material-icons">expand_less</i></button>
     </div>
-    <transition name="fade">
+    <transition name="expand">
       <div class="row expandable" v-if="expand">
         <user-group v-if="redirecttogroup" v-for="group in groups" :group="group" :key="group.id"></user-group>
         <group v-if="!redirecttogroup" v-for="group in groups" :group="group" :key="group.id"></group>
@@ -43,10 +43,10 @@ export default
   max-height: 500px
   overflow: hidden
 
-.fade-enter-active, .fade-leave-active
+.expand-enter-active, .expand-leave-active
   transition: max-height .4s
 
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */
+.expand-enter, .expand-leave-to
   max-height: 0
 
 .arrow-down
