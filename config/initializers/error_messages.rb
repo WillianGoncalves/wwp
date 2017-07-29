e7c8dccb107731dec49e0679fc88ef75902509cd
Willian <w.goncalves91@gmail.com>
2017-07-29 11:22:39 -0300

    Implement translations and error messages.

---
+++ config/initializers/error_messages.rb
@@ -0,0 +1,8 @@
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /\<label/
+    html_tag
+  else
+    errors = Array(instance.error_message).join(',')
+    %(#{html_tag}<span class="error">#{errors}</span>).html_safe
+  end
+end
