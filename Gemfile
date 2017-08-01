7abfc97c48e88d12dd6aa712ec80bf91e0561b8f
Willian <w.goncalves91@gmail.com>
2017-08-01 20:10:29 -0300

    Migrate rails 5.0 to 5.1.

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.2'
+gem 'rails', '~> 5.1'
 gem 'pg'
 gem 'pg_search'
 gem 'devise'
--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Wwp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_record.time_zone_aware_types = [:datetime, :time]
-
     config.i18n.available_locales = [:en, 'pt-BR']
     config.i18n.default_locale = 'pt-BR'
   end
