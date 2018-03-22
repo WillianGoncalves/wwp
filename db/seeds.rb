f3d6a7f9509912461fcfd1f035f2ff9f80f9646e
Willian <w.goncalves91@gmail.com>
2018-03-21 23:06:48 -0300

    Creates colors only if they don't exist.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
-color_list = [
-  ["red", "#D32F2F"],
-  ["pink", "#F06292"],
-  ["purple", "#9C27B0"],
-  ["deep_purple", "#5E35B1"],
-  ["indigo", "#1A237E"],
-  ["blue", "#1976D2"],
-  ["light_blue", "#4FC3F7"],
-  ["teal", "#00796B"],
-  ["green", "#388E3C"],
-  ["light_green", "#9CCC65"],
-  ["yellow", "#FFEB3B"],
-  ["amber", "#FFC400"],
-  ["orange", "#EF6C00"],
-  ["brown", "#5D4037"]
-]
+unless Color.any?
+  color_list = [
+    ["red", "#D32F2F"],
+    ["pink", "#F06292"],
+    ["purple", "#9C27B0"],
+    ["deep_purple", "#5E35B1"],
+    ["indigo", "#1A237E"],
+    ["blue", "#1976D2"],
+    ["light_blue", "#4FC3F7"],
+    ["teal", "#00796B"],
+    ["green", "#388E3C"],
+    ["light_green", "#9CCC65"],
+    ["yellow", "#FFEB3B"],
+    ["amber", "#FFC400"],
+    ["orange", "#EF6C00"],
+    ["brown", "#5D4037"]
+  ]
 
-#TODO executar apenas se não houver colors
-color_list.each do |name, code|
-  Color.create(name: name, code: code)
+  color_list.each do |name, code|
+    Color.create(name: name, code: code)
+  end
+else
+  puts "Colors already populated"
 end
