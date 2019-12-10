124758c99ec12fb840747184b7dda84282742f8a
Willian <w.goncalves91@gmail.com>
2019-12-10 20:25:15 -0300

    Remove old songs pagination implementation from rails

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,33 +1,12 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
   before_action :set_group
-  before_action :pagination_params, only: [:index]
   before_action do
     require_group_member(@group)
   end
 
   def index
     @songs = @group.songs.order(:title)
-
-    #if search_params[:tags_ids].present?
-      #tags_ids = (search_params[:tags_ids]).split(',')
-      #@songs = @songs.joins(:tags).where('tags.id IN (?)', tags_ids).distinct
-    #end
-
-    #if search_params[:query].present?
-      #q = "%#{search_params[:query].downcase}%"
-      #@songs = @songs.where('lower(title) LIKE ? OR lower(author) LIKE ?', q, q)
-    #end
-
-    #calculate_page_count(@songs.count)
-
-    #@songs = @songs.limit(@items_per_page).offset((@page - 1) * @items_per_page)
-
-    respond_to do |format|
-      format.html
-      format.json
-      format.js { render partial: 'songs/songs_list' }
-    end
   end
 
   def show
@@ -75,9 +54,5 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title, :author, :tone, tag_ids: [])
-  end
-
-  def search_params
-    params.permit(:tags_ids, :query, :page, :items_per_page)
   end
 end
